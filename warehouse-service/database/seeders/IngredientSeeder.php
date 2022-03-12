5df34296e0343ff73396efe13577c0bbddd8e2e8
Ivan Aquino <ivnaqn@gmail.com>
2022-03-12 13:50:23 -0600

    WAREHOUSE: added model Ingredent and seeder, changed db host in .env.example

---
+++ warehouse-service/database/seeders/IngredientSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ingredient;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class IngredientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $ingredients = [
+            'tomato',
+            'lemon',
+            'potato',
+            'rice',
+            'ketchup',
+            'lettuce',
+            'onion',
+            'cheese',
+            'meat',
+            'chicken',
+        ];
+
+        foreach($ingredients as $ingredient) {
+            Ingredient::create([
+                'name' => $ingredient,
+                'stock' => 5,
+            ]);
+        }
+    }
+}
