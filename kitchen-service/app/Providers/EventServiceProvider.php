93afb81f4e53650857253bd5c78e9e9040b5eef9
Ivan Aquino <ivnaqn@gmail.com>
2022-03-14 00:04:05 -0600

    KITCHEN: Added info for warehouse queue, handle event when ingredients are ready to kitchen

--- kitchen-service/app/Providers/EventServiceProvider.php
+++ kitchen-service/app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Jobs\IngredientsReadyForOrderJob;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,7 +28,10 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bindMethod(
+            [IngredientsReadyForOrderJob::class, 'handle'],
+            fn($job) => $job->handle()
+        );
     }
 
     /**
